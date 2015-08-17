f6e38b16fa293b56ae46083feb2960b65ce3752b
mohitbora-optimus <imohitbora@gmail.com>
2015-08-17 19:36:54 +0530

    Assignment 4

---
+++ Assignment4/Assignment4/StringTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class StringTest
+{
+    static void Main(string[] args)
+    {
+        string FirstName = "rahul";
+        string LastName = "singh";
+
+        Console.WriteLine(FirstName + " " + LastName);
+        Console.WriteLine(String.Concat(FirstName," ", LastName));
+        if (FirstName.Contains(LastName))
+            Console.WriteLine("\n FirstName is Contained within Lastname");
+        else
+            Console.WriteLine("\n FirstName is not Contained within Lastname");
+
+        Console.WriteLine(FirstName.ToUpper() + " "+ LastName.ToLower());
+
+        Console.WriteLine(FirstName.Substring(2));
+
+        Console.WriteLine(String.Compare(FirstName, LastName));
+        Console.ReadKey();
+    }
+}
