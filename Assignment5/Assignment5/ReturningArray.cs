e8b4076282796819510fe781cebed76e479bff08
mohitbora-optimus <imohitbora@gmail.com>
2015-08-18 15:10:58 +0530

    Assignment 5

---
+++ Assignment5/Assignment5/ReturningArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ReturningArray
+{
+    public string[] GetStringArray()
+    {
+        string[] Names = new string[] { "Tiwari", "Bhatti", "Jha", "rehan" };
+        return Names;
+    }
+
+
+    public static void Main(String[] args)
+    {
+        ReturningArray p = new ReturningArray();
+        foreach (string names in p.GetStringArray())
+            Console.WriteLine(names);
+        Console.ReadKey();
+    }
+}
+        
