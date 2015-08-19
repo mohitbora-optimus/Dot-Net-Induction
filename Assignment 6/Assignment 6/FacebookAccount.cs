f3392b1cae19a4b5c523725d9ef5b89f3a4509c6
mohitbora-optimus <imohitbora@gmail.com>
2015-08-19 10:31:54 +0530

    Assignment 6

---
+++ Assignment 6/Assignment 6/FacebookAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment_6
+{
+    class FacebookAccount
+    {
+        private string _userName;
+        private int _userId;
+        private int _age;
+        private string _designation;
+
+        public void GetDetails()
+        {
+            Console.WriteLine("..........Enter the details of the user ............ ");
+
+            Console.Write("Enter User Name: \t");
+            _userName = Console.ReadLine();
+
+            Console.Write("Enter User Id number: \t");
+            _userId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter User Age: \t");
+            _age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter User Designation: \t");
+            _designation = Console.ReadLine();
+
+        }
+        
+        public void PutDetails()
+        {
+            Console.WriteLine(" \n \n \t ..........User Details....... ");
+            Console.WriteLine("\t\t User Name: \t {0}", this._userName);
+            Console.WriteLine("\t\t User Id: \t {0}", this._userId);
+            Console.WriteLine("\t\t User Age: \t {0}", this._age);
+            Console.WriteLine("\t\t User Designation: \t {0}", this._designation);        
+        }
+       
+    }
+}
---
+++ Assignment 6/Assignment 6/program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Assignment_6;
+
+namespace Assignment_6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            FacebookAccount[] FacebookUser = new FacebookAccount[5];
+            int i ;
+
+            for(i = 0; i < 5; i++)
+            {
+                char choice;
+
+                Console.WriteLine("Want to insert data ..... (y/n) :");
+                choice = Convert.ToChar(Console.Read());
+
+                if (choice == 'y' || choice == 'Y')
+                {
+                }
+                else
+                    break;
+
+                FacebookUser[i] = new FacebookAccount();
+
+                FacebookUser[i].GetDetails();
+            }
+
+
+            for (int j = 0; j < i /*sizeof(FacebookUser)/sizeof(FacebookAccount)*/; j++)
+            {
+                FacebookUser[j].PutDetails();
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
