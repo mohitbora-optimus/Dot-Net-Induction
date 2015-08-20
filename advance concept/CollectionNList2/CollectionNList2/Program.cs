8d957fef4a077652ef1e63ae339f04de60137dc8
mohitbora-optimus <imohitbora@gmail.com>
2015-08-20 18:47:02 +0530

    assignment 13

---
+++ advance concept/CollectionNList2/CollectionNList2/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections;
+//using Assignment_11;
+
+namespace CollectionNList2
+{
+   public class Program
+    {
+        public static void GetDetails(Vehicle newVehicle)
+        {
+            Console.WriteLine();
+            Random rand = new Random();
+            newVehicle.Id = rand.Next(1000, 9999);
+            Console.WriteLine();
+            Console.Write(" ...... 1. Enter Vehicle Type : \t \t");
+            newVehicle.VehicleType = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write(" ...... 2. Enter Manufacturer Name : \t \t");
+            newVehicle.Manufacturer = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+       
+        static void Main(string[] args)
+        {
+            ArrayList Vehicle = new ArrayList();
+            for (int i = 0; i < 2; i++)
+            {
+                Vehicle newVehicle = new Vehicle();
+                Console.WriteLine("------------------- Enter {0} vehicle details ----------------", i + 1);
+                Program.GetDetails(newVehicle);
+                Console.WriteLine("------------------------------------------------------------------");
+
+                Vehicle.Add(newVehicle);
+            }
+
+            for (int i = 0; i < Vehicle.Count; i++)
+            {
+ 
+                Console.WriteLine(Vehicle[i]);
+               // Vehicle[i].PrintDetails();
+            }
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
---
+++ advance concept/CollectionNList2/CollectionNList2/Vehicle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionNList2
+{
+    //public enum CountVehicle { First = 1, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth };
+
+    public class Vehicle : IComparable<Vehicle>, IEquatable<Vehicle>
+    {
+        private string _vehicleType;
+        private int _vehicleId;
+        private string _manufacturer;
+
+        public string VehicleType
+        {
+            get
+            {
+                return _vehicleType;
+            }
+            set
+            {
+                _vehicleType = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _vehicleId;
+            }
+            set
+            {
+                _vehicleId = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return _manufacturer;
+            }
+            set
+            {
+                _manufacturer = value;
+            }
+        }
+
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("....... vehicle details .......");
+            Console.WriteLine(" ... 1. Vehicle Id : \t \t{0}", _vehicleId);
+            Console.WriteLine(" ... 2. Vehicle Type : \t \t {0}", _vehicleType);
+            Console.WriteLine(" ... 3. Manufacturer Name : \t \t {0}", _manufacturer);
+            Console.WriteLine("\n");
+
+        }
+
+        public int CompareTo(Vehicle obj)
+        {
+            // Vehicle v = (Vehicle)obj;
+            return ((this.Id == obj.Id) ? 0 : ((this.Id > obj.Id) ? 1 : -1));
+
+        }
+
+        public bool Equals(Vehicle obj)
+        {
+            if ((this.Id == obj.Id) && (string.Compare(obj.Manufacturer, this.Manufacturer) == 0) && (this.VehicleType == obj.VehicleType))
+                return true;
+            else
+                return false;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("vehicle Id:{0} is of {1} type manufactured by {2}", Id, VehicleType, Manufacturer);
+        }
+    }
+}
