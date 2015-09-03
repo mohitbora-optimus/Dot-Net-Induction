23cff4ba16c231719783fd1ff34ef9add27d5377
mohitbora-optimus <imohitbora@gmail.com>
2015-09-03 09:24:07 +0530

    Learning XPath and XSLT

---
+++ Assignment29/ConsoleApplication1/Xml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    public class Xml
+    {
+        private string _fileName;
+        private string _name;
+        private int _age;
+        private string _address;
+
+        public Xml(string file)
+        {
+            _fileName = file;
+        }
+
+        public void CreateXml()
+        {
+            XmlTextWriter textWriter = new XmlTextWriter(_fileName, Encoding.UTF8);
+
+            textWriter.WriteStartDocument();
+            textWriter.WriteStartElement("ROOT");
+            textWriter.WriteStartElement("Student");
+
+            textWriter.WriteElementString("Name", "AJAY");
+            textWriter.WriteElementString("Age", "15");
+            textWriter.WriteElementString("Branch", "CSE");
+
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteElementString("City", "delhi");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+
+
+            textWriter.WriteStartElement("Student");
+
+            textWriter.WriteElementString("Name", "Vijaty");
+            textWriter.WriteElementString("Age", "15");
+            textWriter.WriteElementString("Branch", "PCM");
+
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteElementString("City", "MUMBAI");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+        }
+
+        public void navigatingXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(_fileName);
+            XmlNode root = doc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("//ROOT/Student");
+
+            foreach (XmlNode node in nodes)
+            {
+                XmlNode name = node.SelectSingleNode("Name");
+                XmlNode age = node.SelectSingleNode("Age");
+                XmlNode branch = node.SelectSingleNode("Branch");
+                Console.WriteLine("Name : {0} \n  Age : {1} \n Branch: {2}", name.InnerText, age.InnerText, branch.InnerText);
+            }
+            Console.ReadKey();
+
+        }
+
+        public void AppendingChild()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(_fileName);
+
+            XmlNode root = doc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("/ROOT/Student");
+            XmlElement College = doc.CreateElement("College");
+            College.InnerText = "Engineering";
+
+
+            foreach(XmlNode node in nodes)
+            {
+                XmlNode branch = node.SelectSingleNode("Branch");
+
+                if (branch.InnerText.CompareTo("PCM") == 0)
+                {
+                    node.InsertAfter(College,branch);
+                   
+                }
+            }
+            doc.Save(_fileName);
+        }
+    }
+}
---
+++ Assignment29/ConsoleApplication1/XmlService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class XmlService
+    {
+        private string fileName ="XmlFile.xml";
+
+        public static void Main(string[] args)
+        {
+            XmlService xmlService = new XmlService();
+
+            Xml XmlObject = new Xml(xmlService.fileName);
+            XmlObject.CreateXml();
+
+            XmlObject.navigatingXml();
+
+            XmlObject.AppendingChild();
+        }
+    }
+}
